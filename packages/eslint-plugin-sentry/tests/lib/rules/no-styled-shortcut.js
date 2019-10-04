c9964b96efbb69026a79034e344658dd6a0b941b
Evan Purkhiser <evanpurkhiser@gmail.com>
2019-10-03 18:19:58 -0700

    feat(plugin): Add a plugin for custom rules

---
+++ packages/eslint-plugin-sentry/tests/lib/rules/no-styled-shortcut.js
@@ -0,0 +1,46 @@
+/**
+ * @fileoverview disallow styled shorthand
+ * @author billy vong
+ */
+'use strict';
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require('../../../lib/rules/no-styled-shortcut'),
+  path = require('path'),
+  RuleTester = require('eslint').RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+RuleTester.setDefaultConfig({
+  parserOptions: {
+    ecmaVersion: 6,
+  },
+});
+var ruleTester = new RuleTester();
+
+ruleTester.run('no-styled-shortcut', rule, {
+  valid: [
+    // give me some code that won't trigger a warning
+    {
+      code: "var Test = styled('div')",
+    },
+  ],
+
+  invalid: [
+    {
+      code: 'var Test = styled.div``;',
+      errors: [
+        {
+          message:
+            'Do not use the shorthand/member expression style of styled. Use the function call syntax instead: styled(div).',
+          type: 'TaggedTemplateExpression',
+        },
+      ],
+    },
+  ],
+});
