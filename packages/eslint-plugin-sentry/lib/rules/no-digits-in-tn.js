0e2ab3f731f48080d87c8d11bf98210e77bfcc6b
Matej Minar <matej.minar@sentry.io>
2020-01-23 09:52:48 +0100

    feat(rule): Adds rule to disallow %d within tn()

---
+++ packages/eslint-plugin-sentry/lib/rules/no-digits-in-tn.js
@@ -0,0 +1,61 @@
+/**
+ * @fileoverview Rule to disallow using %d within tn()
+ * @author Matej Minar
+ */
+'use strict';
+
+//------------------------------------------------------------------------------
+// Rule Definition
+//------------------------------------------------------------------------------
+
+module.exports = {
+  meta: {
+    docs: {
+      description: 'disallow using %d within tn()',
+      category: 'Best Practices',
+      recommended: true,
+    },
+    fixable: 'code',
+    schema: [],
+  },
+  create: function(context) {
+    // variables should be defined here
+
+    //----------------------------------------------------------------------
+    // Helpers
+    //----------------------------------------------------------------------
+
+    // any helper functions should go here or else delete this section
+
+    //----------------------------------------------------------------------
+    // Public
+    //----------------------------------------------------------------------
+
+    return {
+      CallExpression(node) {
+        if (node.callee.name === 'tn') {
+          node.arguments.forEach(argument => {
+            if (
+              argument.value &&
+              argument.value.includes &&
+              argument.value.includes('%d')
+            ) {
+              context.report({
+                node,
+                message: `Do not use '%d' within 'tn()'. Use '%s' instead.`,
+                fix(fixer) {
+                  return [
+                    fixer.replaceTextRange(
+                      [argument.range[0] + 1, argument.range[1] - 1],
+                      argument.value.replace(/%d/g, '%s')
+                    ),
+                  ];
+                },
+              });
+            }
+          });
+        }
+      },
+    };
+  },
+};
