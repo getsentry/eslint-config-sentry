bdecda3456bc8b4a588fd4cadcf428a3181c80af
Matej Minar <matej.minar@sentry.io>
2021-11-03 19:25:05 +0100

    move in some rules from sentry

--- packages/eslint-config-sentry-react/index.js
+++ packages/eslint-config-sentry-react/index.js
@@ -17,6 +17,8 @@ module.exports = {
      */
     // highlights literals in JSX components w/o translation tags
     'getsentry/jsx-needs-il8n': ['off'],
+    'testing-library/render-result-naming-convention': 'off',
+    'testing-library/prefer-screen-queries': 'off',
   },
 
   /**
